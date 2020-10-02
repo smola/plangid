565688b972857342fb0a79817140e95cd7de0311
Santiago M. Mola <santi@mola.io>
2020-10-02 18:34:31 +0200

    Add some more tests, use pytest fixtures

--- plangid/tests/test_pipeline.py
+++ plangid/tests/test_pipeline.py
@@ -1,11 +1,41 @@
+import pandas as pd
+import pytest
+
 from plangid.dataset import Dataset
 from plangid.pipeline import LanguagePipeline
 
 
-def test_fit_predict():
-    dataset = Dataset()
-    df = dataset.to_df()
-    df = df.head(n=100)
-    lp = LanguagePipeline()
-    lp.fit(df)
-    lp.predict(df)
+@pytest.fixture(scope="module")
+def dataset() -> Dataset:
+    return Dataset()
+
+
+@pytest.fixture(scope="module")
+def df(dataset: Dataset) -> pd.DataFrame:
+    return dataset.to_df()
+
+
+@pytest.fixture(scope="function")
+def lp() -> LanguagePipeline:
+    return LanguagePipeline()
+
+
+@pytest.fixture(scope="module")
+def fitted_lp(df: pd.DataFrame) -> LanguagePipeline:
+    return LanguagePipeline().fit(df.head(n=100))
+
+
+def test_predict(fitted_lp: LanguagePipeline, df: pd.DataFrame) -> None:
+    assert fitted_lp.predict(df.head(n=100)) is not None
+
+
+def test_predict_proba(fitted_lp: LanguagePipeline, df: pd.DataFrame) -> None:
+    assert fitted_lp.predict_proba(df.head(n=100)) is not None
+
+
+def test_fit_save_load(tmp_path, fitted_lp, df):
+    model_path = tmp_path / "model"
+    fitted_lp.save(model_path)
+    new_lp = LanguagePipeline.load(model_path)
+    assert isinstance(new_lp, LanguagePipeline)
+    assert new_lp.predict(df.head(n=100)) is not None
