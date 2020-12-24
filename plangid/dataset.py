0ee1d057b3bbf929f5e9ff6856d597a4b2f817f7
Santiago M. Mola <santi@mola.io>
2020-12-24 14:24:57 +0100

    Add support for group names in dataset (#26)

--- plangid/dataset.py
+++ plangid/dataset.py
@@ -29,25 +29,38 @@ class Dataset:
 
     def samples(self):
         dataset_path = os.path.join(self.path, "dataset.yml")
+        meta_path = os.path.join(self.path, "meta.yml")
+        with open(meta_path, "r") as f:
+            meta = yaml.load(f, Loader=Loader)
         with open(dataset_path, "r") as f:
             dataset = yaml.load(f, Loader=Loader)
-        return ((k, v["annotations"]["vote"]) for k, v in dataset["files"].items())
+        samples = []
+        for path, data in dataset["files"].items():
+            class_name = data["annotations"]["vote"]
+            group_name = "Unknown"
+            if class_name != "Unknown":
+                group_name = meta["languages"][class_name].get("group", class_name)
+            samples.append((path, group_name, class_name))
+        return samples
 
     def to_df(self):
         class_name = []
+        group_name = []
         filename = []
         content = []
         path = []
-        for p, c in self.samples():
+        for p, g, c in self.samples():
             if c == "Unknown":
                 continue
             class_name.append(c)
+            group_name.append(g)
             path.append(p)
             s = Dataset.load_sample(os.path.join(self.path, p))
             filename.append(s["filename"])
             content.append(s["content"])
         df = pd.DataFrame()
         df["class_name"] = pd.Series(class_name)
+        df["group_name"] = pd.Series(group_name)
         df["filename"] = pd.Series(filename)
         df["content"] = pd.Series(content)
         df["path"] = pd.Series(path)
--- plangid/tests/test_dataset.py
+++ plangid/tests/test_dataset.py
@@ -5,13 +5,13 @@ def test_samples():
     dataset = Dataset()
     samples = list(dataset.samples())
     assert len(samples) > 0
-    assert all([len(s) == 2 for s in samples])
+    assert all([len(s) == 3 for s in samples])
 
 
 def test_to_df():
     dataset = Dataset()
     df = dataset.to_df()
-    assert set(["class_name", "class_index", "content", "path", "filename"]) == set(
-        df.columns
-    )
+    assert set(
+        ["group_name", "class_name", "class_index", "content", "path", "filename"]
+    ) == set(df.columns)
     assert len(df["class_index"].unique()) - 1 == df["class_index"].max()
